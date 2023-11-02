42b8a4f998b3928d38d70537f425e278332e31e9
vulinana <sm.vulinana@gmail.com>
2023-11-02 14:21:59 +0100

    created repository and repository member model

---
+++ Domain/Repositories/RepositoryMember.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Auth;
+using Domain.Organizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repositories
+{
+    public class RepositoryMember
+    {
+        public Guid Id { get; private set; }
+        public User Member { get; private set; }
+        public Repository Repository { get; private set; }
+        public Guid RepositoryId { get; private set; }
+
+        public RepositoryMember()
+        {
+        }
+    }
+}
---
+++ Infrastructure/Migrations/20231102131041_AddedRepositoryAndRepositoryMember.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRepositoryAndRepositoryMember : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Repositories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    OwnerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    IsPrivate = table.Column<bool>(type: "boolean", nullable: false),
+                    OrganizationId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Repositories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Repositories_Organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "Organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Repositories_Users_OwnerId",
+                        column: x => x.OwnerId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RepositoryMembers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MemberId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RepositoryId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepositoryMembers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RepositoryMembers_Repositories_RepositoryId",
+                        column: x => x.RepositoryId,
+                        principalTable: "Repositories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RepositoryMembers_Users_MemberId",
+                        column: x => x.MemberId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RepositoryUser",
+                columns: table => new
+                {
+                    PendingMembersId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PendingRepositoriesId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RepositoryUser", x => new { x.PendingMembersId, x.PendingRepositoriesId });
+                    table.ForeignKey(
+                        name: "FK_RepositoryUser_Repositories_PendingRepositoriesId",
+                        column: x => x.PendingRepositoriesId,
+                        principalTable: "Repositories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RepositoryUser_Users_PendingMembersId",
+                        column: x => x.PendingMembersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repositories_OrganizationId",
+                table: "Repositories",
+                column: "OrganizationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Repositories_OwnerId",
+                table: "Repositories",
+                column: "OwnerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RepositoryMembers_MemberId",
+                table: "RepositoryMembers",
+                column: "MemberId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RepositoryMembers_RepositoryId",
+                table: "RepositoryMembers",
+                column: "RepositoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RepositoryUser_PendingRepositoriesId",
+                table: "RepositoryUser",
+                column: "PendingRepositoriesId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RepositoryMembers");
+
+            migrationBuilder.DropTable(
+                name: "RepositoryUser");
+
+            migrationBuilder.DropTable(
+                name: "Repositories");
+        }
+    }
+}
