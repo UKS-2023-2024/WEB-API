b5ee1795c3a33cfbd754cb40527f2ba5d49ee2fa
vulinana <sm.vulinana@gmail.com>
2023-11-04 21:09:08 +0100

    deleted comments

--- Application/Auth/Commands/Update/UpdateUserCommand.cs
+++ Application/Auth/Commands/Update/UpdateUserCommand.cs
@@ -2,19 +2,4 @@
 using Domain.Auth;
 
 namespace Application.Auth.Commands.Update;
-
-
-/*
- * Komanda predstavlja objekat koji prenosi podatke do handlera(DTO).
- * U ovom slucaju potrebno je da naslijedi od ICommand interfejsa,
- * koji sam dodao, kako bi mogao MediatR da je serializuje.
- * Uglavnom nebitno, samo naslijediti svaku komandu sa ICommand.
- * Sto se tice paketiranja. Bukvalno se folder nazove kao operacija,
- * dok se klasama daju puna imena. Primjer:
- * Operacija => Create ili CreateUser
- * Komanda => CreateUserCommand
- * Handler => CreateUserCommandHandler ili CreateUserHandler
- * Mozemo se dogovoriti oko ovih konvencija. Takodje ista stvar vazi i za querije.
- * 
- */
 public sealed record UpdateUserCommand(Guid id, string fullName, string bio, string company, string location, string website, List<SocialAccount> socialAccounts) : ICommand<User>;
