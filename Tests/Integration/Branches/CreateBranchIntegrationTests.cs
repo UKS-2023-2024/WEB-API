f347b061a3e5f59a09bc99f7f91f0c865d783456
vulinana <sm.vulinana@gmail.com>
2023-11-23 23:39:37 +0100

    unit and integration tests for create branch

---
+++ Tests/Integration/Branches/CreateBranchIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Branches.Commands.Create;
+using Application.Milestones.Commands.Create;
+using Domain.Branches;
+using Domain.Milestones;
+using Domain.Repositories.Exceptions;
+using Shouldly;
+using Tests.Integration.Setup;
+
+namespace Tests.Integration.Branches;
+
+[Collection("Sequential")]
+public class CreateBranchIntegrationTests : BaseIntegrationTest
+{
+    public CreateBranchIntegrationTests(TestDatabaseFactory factory) : base(factory)
+    {
+
+    }
+
+    [Fact]
+    async Task CreateBranch_ShouldBeSuccessful_WhenCommandIsValid()
+    {
+        //Arrange
+        var command = new CreateBranchCommand("branch1", new Guid("8e9b1cc1-35d3-4bf2-9f2c-5e00a21d14a5"), false);
+
+        //Act
+        var branchId = await _sender.Send(command);
+        Branch branch = await _context.Branches.FindAsync(branchId);
+
+        //Assert
+        branchId.ShouldBeOfType<Guid>();
+        branch.ShouldNotBeNull();
+    }
+}
---
+++ Tests/Unit/Branches/CreateBranchUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using Application.Branches.Commands.Create;
+using Application.Milestones.Commands.Create;
+using Domain.Auth;
+using Domain.Branches;
+using Domain.Branches.Interfaces;
+using Domain.Milestones;
+using Domain.Milestones.Interfaces;
+using Domain.Repositories;
+using Domain.Repositories.Exceptions;
+using Domain.Repositories.Interfaces;
+using Moq;
+using Shouldly;
+
+namespace Tests.Unit.Branches;
+
+public class CreateBranchUnitTests
+{
+    private readonly Mock<IBranchRepository> _branchRepositoryMock;
+
+    public CreateBranchUnitTests()
+    {
+        _branchRepositoryMock = new();
+    }
+
+    [Fact]
+    public async void CreateBranch_ShouldBeSuccessful_WhenCommandIsValid()
+    {
+        //Arrange
+        var command = new CreateBranchCommand("branch1", Guid.Parse("705a6c69-5b51-4156-b4cc-71e8dd111579"), false);
+        Branch branch = Branch.Create("branch1", Guid.Parse("705a6c69-5b51-4156-b4cc-71e8dd111579"), false);
+       
+        _branchRepositoryMock.Setup(x => x.Create(It.IsAny<Branch>()))
+            .ReturnsAsync(branch);
+   
+        var handler = new CreateBranchCommandHandler(_branchRepositoryMock.Object);
+
+        //Act
+        Guid branchId = await handler.Handle(command, default);
+
+        //Assert
+        branchId.ShouldBeOfType<Guid>();
+    }
+
+}
