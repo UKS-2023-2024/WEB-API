c44b8f262796617b8addee6c4aa7a576b052fadd
vulinana <sm.vulinana@gmail.com>
2023-11-24 19:51:36 +0100

    make branch default

--- Domain/Branches/Interfaces/IBranchRepository.cs
+++ Domain/Branches/Interfaces/IBranchRepository.cs
@@ -5,4 +5,5 @@ namespace Domain.Branches.Interfaces;
 public interface IBranchRepository : IBaseRepository<Branch>
 {
     public Task<Branch?> FindByNameAndRepositoryId(string name, Guid repositoryId);
+    public Task<Branch?> FindByRepositoryIdAndIsDefault(Guid repositoryId, bool isDefault);
 }
--- Infrastructure/Branches/BranchRepository.cs
+++ Infrastructure/Branches/BranchRepository.cs
@@ -3,6 +3,7 @@ using Domain.Branches.Interfaces;
 using Infrastructure.Persistence;
 using Infrastructure.Shared.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace Infrastructure.Branches
 {
@@ -19,6 +20,13 @@ namespace Infrastructure.Branches
             return await _context.Branches
                 .Where(b => b.Name.Equals(name) && b.RepositoryId.Equals(repositoryId))
                 .FirstOrDefaultAsync();
+        }
+
+        public async Task<Branch?> FindByRepositoryIdAndIsDefault(Guid repositoryId, bool isDefault)
+        {
+            return await _context.Branches
+                    .Where(b => b.RepositoryId.Equals(repositoryId) && b.IsDefault == isDefault)
+                    .FirstOrDefaultAsync();
         }
     }
 }
