adbf0804e7bf1832e7b200e657563a76014c6aae
vulinana <sm.vulinana@gmail.com>
2024-01-24 00:30:51 +0100

    unwatch repository

---
+++ Application/Repositories/Commands/WatchingRepository/UnwatchRepository/UnwatchRepositoryCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Shared;
+using Domain.Repositories;
+using Domain.Repositories.Exceptions;
+using Domain.Repositories.Interfaces;
+
+namespace Application.Repositories.Commands.WatchingRepository.UnwatchRepository;
+
+public class UnwatchRepositoryCommandHandler: ICommandHandler<UnwatchRepositoryCommand>
+{
+    private readonly IRepositoryRepository _repositoryRepository;
+    
+    public UnwatchRepositoryCommandHandler(IRepositoryRepository repositoryRepository)
+    {
+        _repositoryRepository = repositoryRepository;
+    }
+
+    public async Task Handle(UnwatchRepositoryCommand request, CancellationToken cancellationToken)
+    {
+        var repository =  _repositoryRepository.Find(request.RepositoryId);
+        
+        Repository.ThrowIfDoesntExist(repository);
+        repository!.ThrowIfNotWatchedBy(request.User.Id);
+
+        repository.RemoveFromWatchedBy(request.User);
+        _repositoryRepository.Update(repository);
+    }
+}
