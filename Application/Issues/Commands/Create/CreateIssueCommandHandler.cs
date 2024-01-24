1a189d22a39c0bc19d21099e9ae1241b42c70da4
vulinana <sm.vulinana@gmail.com>
2024-01-24 23:13:32 +0100

    send notification to repository watchers when openning an issue

--- Application/Issues/Commands/Create/CreateIssueCommandHandler.cs
+++ Application/Issues/Commands/Create/CreateIssueCommandHandler.cs
@@ -4,6 +4,7 @@ using Domain.Auth.Interfaces;
 using Domain.Repositories;
 using Domain.Repositories.Exceptions;
 using Domain.Repositories.Interfaces;
+using Domain.Shared.Interfaces;
 using Domain.Tasks;
 using Domain.Tasks.Enums;
 using Domain.Tasks.Interfaces;
@@ -18,9 +19,11 @@ public class CreateIssueCommandHandler : ICommandHandler<CreateIssueCommand, Gui
     private readonly IRepositoryRepository _repositoryRepository;
     private readonly IUserRepository _userRepository;
     private readonly ILabelRepository _labelRepository;
+    private readonly IEmailService _emailService;
+
     public CreateIssueCommandHandler(IRepositoryMemberRepository repositoryMemberRepository, ITaskRepository taskRepository,
         IRepositoryRepository repositoryRepository, IIssueRepository issueRepository,
-        IUserRepository userRepository, ILabelRepository labelRepository)
+        IUserRepository userRepository, ILabelRepository labelRepository, IEmailService emailService)
     {
         _repositoryMemberRepository = repositoryMemberRepository;
         _taskRepository = taskRepository;
@@ -28,6 +31,7 @@ public class CreateIssueCommandHandler : ICommandHandler<CreateIssueCommand, Gui
         _issueRepository = issueRepository;
         _userRepository = userRepository;
         _labelRepository = labelRepository;
+        _emailService = emailService;
     }
 
     public async Task<Guid> Handle(CreateIssueCommand request, CancellationToken cancellationToken)
@@ -48,6 +52,12 @@ public class CreateIssueCommandHandler : ICommandHandler<CreateIssueCommand, Gui
             repository, creator, assignees, labels, request.MilestoneId);
         Issue createdIssue = await _issueRepository.Create(issue);
         
+
+        foreach(User watcher in repository.WatchedBy)
+        {
+            await _emailService.SendNotificationIssueIsOpen(watcher, issue, repository.Name);
+        }
+
         return createdIssue.Id;
     }
 }
--- Infrastructure/Shared/Email/SendgridEmailService.cs
+++ Infrastructure/Shared/Email/SendgridEmailService.cs
@@ -1,3 +1,4 @@
+using Domain.Auth;
 using Domain.Shared.Interfaces;
 using Microsoft.Extensions.Configuration;
 using SendGrid;
@@ -32,6 +33,23 @@ public class SendgridEmailService: IEmailService
         EmailAddress to = new EmailAddress(email);
         const string subject = "You have been invited!";
         string plainTextContent = $"You have been invited to repository <a>{link}";
+        string htmlContent = "";
+        SendGridMessage message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+        await _sendgrid.SendEmailAsync(message);
+    }
+
+    public async Task SendNotificationIssueIsOpen(User user, Domain.Tasks.Issue issue, string repositoryName)
+    {
+        EmailAddress from = new EmailAddress(_sender);
+        EmailAddress to = new EmailAddress(user.PrimaryEmail);
+        string subject = $"[Github] New Issue opened in {repositoryName}";
+        string plainTextContent = $"Hello {user.Username} <br><br>" +
+                                   $"A new issue has been opened in the repository {repositoryName}<br><br>" +
+                                   $"Title: {issue.Title} <br>"  +
+                                   $"Description: {issue.Description}<br>" +
+                                   $"Opened by: {issue.Creator?.Username}<br><br>" +
+                                   $"You are receiving this email because you are watching the repository. <br><br>" +
+                                   $"The GitHub Team";
         string htmlContent = "";
         SendGridMessage message = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
         await _sendgrid.SendEmailAsync(message);
