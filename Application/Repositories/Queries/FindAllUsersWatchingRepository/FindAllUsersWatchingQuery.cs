e5779d27bd1c8c983ca0945e240a27895a4cb633
vulinana <sm.vulinana@gmail.com>
2024-01-24 01:27:12 +0100

    fetch users watching repo

---
+++ Application/Repositories/Queries/FindAllUsersWatchingRepository/FindAllUsersWatchingQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Shared;
+using Domain.Auth;
+
+namespace Application.Repositories.Queries.FindAllUsersWatchingRepository;
+
+public sealed record FindAllUsersWatchingQuery(Guid UserId, Guid RepositoryId) : IQuery<IEnumerable<User>>;
