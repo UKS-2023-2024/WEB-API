c43a55af6f3758962264cf0fe00a83a641c384cb
vulinana <sm.vulinana@gmail.com>
2024-01-26 18:53:19 +0100

    adjusted sending notifications to custom watching

--- Infrastructure/Notifications/Services/NotificationService.cs
+++ Infrastructure/Notifications/Services/NotificationService.cs
@@ -3,6 +3,7 @@ using Domain.Auth.Enums;
 using Domain.Notifications;
 using Domain.Notifications.Interfaces;
 using Domain.Repositories;
+using Domain.Repositories.Enums;
 using Domain.Shared.Interfaces;
 
 
@@ -17,21 +18,32 @@ namespace Infrastructure.Notifications.Services
             _emailService = emailService;
             _notificationRepository = notificationRepository;
         }
-        public Task SendNotification(Repository repository, string subject, string message)
+        public Task SendNotification(Repository repository, string subject, string message, NotificationType notificationType)
         {
             foreach (RepositoryWatcher watcher in repository.WatchedBy)
             {
-                if (watcher.User.NotificationPreferences == NotificationPreferences.EMAIL || watcher.User.NotificationPreferences == NotificationPreferences.BOTH)
-                {
-                    _emailService.SendNotificationToRepositoryWatcher(watcher.User, subject, message);
-                }
-                if (watcher.User.NotificationPreferences == NotificationPreferences.GITHUB || watcher.User.NotificationPreferences == NotificationPreferences.BOTH)
-                {
-                    Notification notification = Notification.Create(message, watcher.User, DateTime.UtcNow);
-                   _notificationRepository.Create(notification);
+                if (watcher.WatchingPreferences == WatchingPreferences.Ignore) return Task.CompletedTask;
+                if (ShouldUserBeNotified(watcher, notificationType)) 
+                { 
+                    if (watcher.User.NotificationPreferences == NotificationPreferences.EMAIL || watcher.User.NotificationPreferences == NotificationPreferences.BOTH)
+                    {
+                        _emailService.SendNotificationToRepositoryWatcher(watcher.User, subject, message);
+                    }
+                    if (watcher.User.NotificationPreferences == NotificationPreferences.GITHUB || watcher.User.NotificationPreferences == NotificationPreferences.BOTH)
+                    {
+                        Notification notification = Notification.Create(message, watcher.User, DateTime.UtcNow);
+                        _notificationRepository.Create(notification);
+                    }
                 }
             }
             return Task.CompletedTask;
+        }
+
+        private bool ShouldUserBeNotified(RepositoryWatcher watcher, NotificationType notificationType)
+        {
+            WatchingPreferences pref = watcher.WatchingPreferences;
+            if (pref == WatchingPreferences.AllActivity || pref == WatchingPreferences.IssuesAndPullRequests) return true;
+            return pref.ToString() == notificationType.ToString();
         }
     }
 }
